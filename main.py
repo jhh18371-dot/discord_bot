533d99e930138783a80ddb9223a1bf0ca018bb57
jhh18371-dot <jhh18371@gmail.com>
2025-12-01 02:35:27 +0900

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import discord
+from discord.ext import tasks, commands
+import datetime
 
+TOKEN = "여기에_네_토큰_넣기"
+CHANNEL_ID = 1444710933124354158
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+KST = datetime.
